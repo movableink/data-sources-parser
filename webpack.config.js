c8d22a2ce774fa6785d151bb7e324bd04650f5b0
Spencer Price <spencer516@gmail.com>
2018-01-29 09:13:52 -0500

    remove superfluous webpack stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,8 @@
 const path = require('path');
-const version = require('./package.json').version;
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const uglifier = new UglifyJsPlugin({
-  sourceMap: true,
-  uglifyOptions: {
-    // Necessary because of Safari 10 bug
-    // https://bugs.webkit.org/show_bug.cgi?id=171041
-    mangle: {
-      safari10: true
-    }
-  }
-});
-
 const isDevelopment = process.env.NODE_ENV === 'development';
-const plugins = isDevelopment ? [] : [uglifier];
-const filename = 'html-normalizer.js';
+const filename = 'data-sources-parser.js';
 
 module.exports = {
-  plugins,
   entry: ['./src/index.js'],
   output: {
     filename,
@@ -28,15 +13,5 @@ module.exports = {
     port: 1215,
     host: 'movableink.localhost'
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          'babel-loader'
-        ]
-      }
-    ]
-  }
+  module: {}
 };
