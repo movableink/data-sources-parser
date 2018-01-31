d10542ab65c04e02f5db6a1e575f1e74d949504d
Spencer Price <spencer516@gmail.com>
2018-01-31 09:30:54 -0500

    remove parser/mappers as it is a data sources concern

--- src/parser/html.js
+++
@@ -1,8 +0,0 @@
-export default function parseHTML(rawHTML) {
-  if (rawHTML.match(/\<[a-zA-z]+[^>]*>/)) {
-    return rawHTML.trim();
-  } else {
-    throw new Error('Passed string does not contain HTML');
-  }
-}
-
